ecd52ab3e8bf3c79441c11b686cf9ad9eabe8d72
Le Anh Duc <ducvui2003@gmail.com>
2024-09-08 21:28:51 +0700

    hotfix: remove un use import

--- client/src/component/Card.tsx
+++ client/src/component/Card.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { isCurrentBreakpoint } from "../util/mediaQuery";
 import Chip from "./Chip";
 
--- client/src/section/Project.tsx
+++ client/src/section/Project.tsx
@@ -1,7 +1,2 @@
-import { useEffect, useState } from "react";
-import Card from "../component/Card";
-import Link from "../component/Link";
-import { getProject } from "../service/database";
-
 const Project = () => {};
 export default Project;
